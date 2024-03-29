fb5e2b72971dfd202fd31104a97a9578c5e4a06e
RalyukRoman <161949010+RalyukRoman@users.noreply.github.com>
2024-03-29 15:25:12 +0200

    all.update

---
+++ graphic_interface.py
@@ -0,0 +1,83 @@
+import PySimpleGUI as sg
+from parser import BooksToScrapeParse
+
+
+def redact_parse(dict_parse: list[dict]) -> list:
+    list_parse_redact: list = []
+    for i in dict_parse:
+        for k in ['link', 'title', 'description']:
+            if k == 'title':
+                list_parse_redact.append(str(i[k]) + ' ---> ' + str(i['name']))
+            elif k == 'link':
+                list_parse_redact.append(str(i[k]) + '\n')
+            else:
+                list_parse_redact.append(str(i[k]))
+        list_parse_redact.append('\n-----------------------\n')
+    return list_parse_redact
+
+
+tabgrp = [[sg.Text("Request input: ", size=(15, 1)), sg.Text("Page input: ", size=(14, 1), key='PAGE_INPUT'),
+           sg.Text("Request now: ", size=(15, 1)), sg.Text("Page now: ", size=(12, 1), key='PAGE_NOW')],
+          [sg.InputText(size=(15, 1)), sg.InputText(size=(15, 1)),
+           sg.Text("", size=(15, 1), key='REQUEST', background_color='White', text_color='Black'),
+           sg.Text("", size=(15, 1), key='PAGE_N', background_color='White', text_color='Black')],
+          [sg.Button('Ok'), sg.Button('Clear'), sg.Checkbox("Redaction", key='redact')],
+          [sg.TabGroup([[
+              sg.Tab('All', [[sg.Output(size=(88, 20), key='ALL')]], background_color='Blue'),
+              sg.Tab('News', [[sg.Output(size=(88, 20), key='NEWS')]], background_color='Green'),
+              sg.Tab('Videos', [[sg.Output(size=(88, 20), key='VIDEOS')]], background_color='Purple'),
+              sg.Tab('Books', [[sg.Output(size=(88, 20), key='BOOKS')]], background_color='Red')]],
+           tab_location='topleft', title_color='White', tab_background_color='Gray',
+           selected_title_color='Yellow', selected_background_color='Gray',
+           border_width=5, enable_events=True, key="tab_group")],
+          [sg.Button('Back'), sg.Button('Next'), sg.Button('Exit')]]
+
+page_n = -1
+dict_tab = {'ALL': '', 'VIDEOS': '&tbm=vid', 'NEWS': '&tbm=nws', 'BOOKS': '&tbm=bks', }
+request = ''
+
+window = sg.Window("Parser", tabgrp, finalize=True)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+
+    if event == 'Ok':
+        request = values[0]
+        if values[1] == '':
+            if page_n == -1:
+                page_n = 0
+        else:
+            try:
+                page_n = int(values[1])
+                window['PAGE_NOW'].update('Page now: ')
+            except:
+                window['PAGE_NOW'].update('ERROR')
+                continue
+    elif event == 'Next':
+        page_n += 1
+    elif event == 'Back' and page_n > 0:
+        page_n -= 1
+    else:
+        page_n = 0
+
+    if event in ['Ok', 'Next', 'Back']:
+        window['PAGE_NOW'].update('Page now: ')
+        window['PAGE_N'].update(page_n)
+
+    if event in ['Ok', 'Next', 'Back'] and request != '':
+        parse = BooksToScrapeParse(url=request)
+        window['REQUEST'].update(request)
+        dict_parse = parse.parse(tab_dict_parse=dict_tab[values['tab_group'].upper()], page=page_n * 10)
+        if dict_parse == []:
+            window[values['tab_group'].upper()].update(value='No result found!')
+        else:
+            if values['redact']:
+                window[values['tab_group'].upper()].update('\n'.join(redact_parse(dict_parse)))
+            else:
+                window[values['tab_group'].upper()].update(value=parse.print(dict_parse))
+    elif event == 'Clear':
+        window[values['tab_group'].upper()].update(value=' ')
+
+window.close()
---
+++ parser.py
@@ -0,0 +1,83 @@
+# pip install requests parsel json
+import json
+import requests
+from parsel import Selector, SelectorList
+from requests import Response
+
+class BooksToScrapeParse():
+    def __init__(self, url: str) -> None:
+        self.url = "https://www.google.com/search?q=" + url
+        self.headers = self.get_headers()
+        self.response = self.make_request()
+        self.selector = self.make_selector()
+        self.parse_dict = {
+            '':        {'title': 'div.yuRUbf div span a h3::text',
+                         'link': 'div.yuRUbf div span a::attr(href)',
+                         'name': 'div.GTRloc.CA5RN span::text',
+                         'description': 'div.VwiC3b.yXK7lf.lVm3ye.r025kc.hJNv6b.Hdw6tb span',
+                         'description_dop': 'div.VwiC3b.yXK7lf.lVm3ye.r025kc.hJNv6b.Hdw6tb::text'},
+            '&tbm=vid': {'title': 'h3.LC20lb.MBeuO.DKV0Md::text',
+                         'link': 'div.nhaZ2c div span a::attr(href)',
+                         'name': 'div.gqF9jc span::text',
+                         'description': 'div.fzUZNc div.ITZIwc::text'},
+            '&tbm=nws': {'title': 'div.n0jPhd.ynAwRc.MBeuO.nDgy9d::text',
+                         'link': 'a.WlydOe::attr(href)',
+                         'name': 'div.MgUUmf.NUnG9d span::text',
+                         'description': 'div.GI74Re.nDgy9d::text'},
+            '&tbm=bks': {'title': 'div.bHexk.Tz5Hvf div span a h3.LC20lb.MBeuO.DKV0Md::text',
+                         'link': 'div.bHexk.Tz5Hvf div span a::attr(href)',
+                         'name': 'div.N96wpd a.fl span::text',
+                         'description': 'div.cmlJmd span span::text',
+                         'description_dop': 'div.cmlJmd.ETWPw span span'}
+                    }
+
+    def get_headers(self) -> dict:
+        return {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        }
+
+    def make_request(self, url: str | None = None) -> Response:
+        if url is None:
+            url = self.url
+        return requests.get(url=url, headers=self.headers)
+
+    def make_selector(self, response: Response | None = None) -> Selector:
+        if response is None:
+            response = self.response
+        selector: Selector = Selector(text=response.text)
+        return selector
+
+    def parse(self, data=None, bool_parse: bool = True, tab_dict_parse: str = '', page: int = 0) -> list[dict]:
+        if data is None:
+            data = []
+        self.selector = self.make_selector(response=self.make_request(url=self.url + tab_dict_parse + f'&start={page}'))
+        if tab_dict_parse != '&tbm=nws':
+            basis: SelectorList = self.selector.css('.dURPMd > div') if bool_parse else self.selector.css('.dURPMd div> div.ULSxyf')
+        else:
+            basis: SelectorList = self.selector.css('.dURPMd div.MjjYud div > div.SoaBEf')
+        for part in basis:
+            if part.css('div.ULSxyf').get() != None and bool_parse:
+                continue
+            title: str = part.css(self.parse_dict[tab_dict_parse]['title']).get()
+            link: str = part.css(self.parse_dict[tab_dict_parse]['link']).get()
+            name: str = part.css(self.parse_dict[tab_dict_parse]['name']).get()
+            description: str = part.css(self.parse_dict[tab_dict_parse]['description']).get()
+            if description in [None, '...', '...', '... (', '... ( '] and tab_dict_parse in ['', '&tbm=bks']:
+                description: str = part.css(self.parse_dict[tab_dict_parse]['description_dop']).get()
+            if description != None:
+                description = description.replace('/', '').replace('<span>', '').replace('<em>', '')
+            if link == None:
+                continue
+            data.append({
+                        'title': title,
+                        'link': link,
+                        'name': name,
+                        'description': description
+                        })
+        if bool_parse and tab_dict_parse == '':
+            return self.parse(data=data, bool_parse=False, tab_dict_parse=tab_dict_parse, page=page)
+        else:
+            return data
+
+    def print(self, data: list[dict]) -> str:
+        return json.dumps(data, indent=2, ensure_ascii=False)
